8827b98304182f6c6b1256b6b08424f84af1a966
Filippo-Crespi <crespifilippo123@gmail.com>
2025-04-11 18:21:46 +0200

    refactor: reorganize Tailwind CSS setup and configuration
    - Moved Tailwind CSS dependencies to devDependencies in package.json
    - Removed old tailwind.config.ts file
    - Created new tailwind.css file with base, components, and utilities
    - Added new tailwind.config.js file with content paths and theme configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,22 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwin.css",
+    configPath: "tailwind.config.js",
+    exposeConfig: true,
+    viewer: true,
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  ssr: true,
+  nitro: {
+    prerender: {
+      crawlLinks: true,
     },
   },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/image", "@nuxt/fonts"],

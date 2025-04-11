6a8ab8f5d500ba238ff93634b54889f0b8fd8d7f
Filippo-Crespi <crespifilippo123@gmail.com>
2025-04-11 18:33:30 +0200

    refactor: remove Tailwind CSS configuration and related files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,27 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-<<<<<<< HEAD
-  tailwindcss: {
-    cssPath: "~/assets/css/tailwin.css",
-    configPath: "tailwind.config.js",
-    exposeConfig: true,
-    viewer: true,
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  ssr: true,
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-    },
-  },
-=======
->>>>>>> parent of e9f9347 (feat: update nuxtjs workflow to include Tailwind CSS support and enhance build process)
   modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/image", "@nuxt/fonts"],
   css: ["/assets/css/main.css"],
 });

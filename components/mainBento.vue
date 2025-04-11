9fe05d14ec332ecaed0dc2f49d1556464d586c57
Filippo-Crespi <crespifilippo123@gmail.com>
2025-04-11 17:34:18 +0200

    feat: update MainBento
    - Removed background color from the main tech stack window
    - Integrated
    - Enhanced learning section with Docker and Node.js icons

--- components/mainBento.vue
+++ components/mainBento.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <div class="parent h-[calc(100vh-4rem)] px-16 py-8">
-      <Window title="My current tech stack" class="col-span-2 row-span-1 bg-red-500">
+      <Window title="My current tech stack" class="col-span-2 row-span-1">
         <div class="h-full flex justify-evenly items-center text-7xl">
           <div class="flex flex-col items-center">
             <Icon name="tabler:brand-html5" />
@@ -37,7 +37,9 @@
       </Window>
       <Window class="col-span-1 row-span-1 bg-blue-500">github</Window>
       <Window class="col-span-1 row-span-2 bg-green-500">?</Window>
-      <Window class="col-span-1 row-span-1 bg-yellow-500">song</Window>
+      <Window class="col-span-1 row-span-1 bg-yellow-500">
+        <TopSong />
+      </Window>
       <Window class="col-span-2 row-span-2 bg-purple-500">
         <div class="half-parent h-full">
           <Window class="col-span-1 row-span-1 bg-cyan-500">img</Window>
@@ -46,7 +48,18 @@
         </div>
       </Window>
       <Window class="col-span-1 row-span-2 bg-pink-500">?</Window>
-      <Window class="col-span-1 row-span-1 bg-orange-500">cur learn</Window>
+      <Window title="I'm currently learning" class="col-span-1 row-span-1">
+        <div class="h-full flex justify-evenly items-center text-7xl">
+          <div class="flex flex-col items-center">
+            <Icon name="tabler:brand-docker" />
+            <span class="text-xl">Docker</span>
+          </div>
+          <div class="flex flex-col items-center">
+            <Icon name="tabler:brand-nodejs" />
+            <span class="text-xl">Node.js</span>
+          </div>
+        </div>
+      </Window>
       <Window class="col-span-1 row-span-1 bg-teal-500">socials</Window>
       <Window class="col-span-2 row-span-1 bg-gray-500">projects</Window>
     </div>
